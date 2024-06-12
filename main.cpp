d7c00d5630aa20703f8acf5defbaff6401253cbe
Parva <parvadesai404@gmail.com>
2024-06-13 02:53:21 +0530

    [Update] game window title

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@ int main()
 {
     // Define the video mode (dimensions)
     sf::VideoMode videoMode = sf::VideoMode(1280, 720);
+    //sf::VideoMode videoMode = *(new sf::VideoMode(800, 600)); sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
 
     // Create a window object with specific dimensions and a title
-    sf::RenderWindow window(videoMode, "SFML Window");
+    sf::RenderWindow window(videoMode, "Game Window");
     //sf::RenderWindow window(sf::VideoMode(1280, 720), "Resizable Window", sf::Style::Resize);
     //sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Fullscreen Window", sf::Style::Fullscreen);
     window.setFramerateLimit(60);
