81db35eb9de796f3f260603a43df3aede2937a40
Parva <parvadesai404@gmail.com>
2024-06-15 01:33:39 +0530

    [Add] player movement

--- main.cpp
+++ main.cpp
@@ -4,38 +4,44 @@ class Player
 {
 private:
     int health = 3;
-    sf::Vector2f position = sf::Vector2f(585.f, 300.f);
-    int movement_speed = 5;
+    sf::Vector2f position = sf::Vector2f(585.0f, 300.0f);
+    float movement_speed = 7.5f;
     int player_score = 0;
 
 public:
     sf::Texture player_texture;
     sf::Sprite player_sprite;
 
-    int getScore() const
+    int GetScore() const
     {
         return player_score;
     }
 
-    void setScore(const int s)
+    void SetScore(const int s)
     {
         player_score = s;
     }
 
-    sf::Vector2f getPosition() const
+    sf::Vector2f GetPosition() const
     {
         return position;
     }
 
-    void takeDamage()
+    float GetMoveSpeed() const
+    {
+        return movement_speed;
+    }
+
+    void TakeDamage()
     {
     }
 
-    void move()
+    void Move(float offsetX)
     {
+        position.x += offsetX;
     }
 
-    void shootBullets()
+    void ShootBullets()
     {
     }
 };
@@ -44,12 +50,8 @@ int main()
 {
     // Define the video mode (dimensions)
     sf::VideoMode videoMode = sf::VideoMode(1280, 720);
-    //sf::VideoMode videoMode = *(new sf::VideoMode(800, 600)); sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
-
     // Create a window object with specific dimensions and a title
     sf::RenderWindow window(videoMode, "Game Window");
-    //sf::RenderWindow window(sf::VideoMode(1280, 720), "Resizable Window", sf::Style::Resize);
-    //sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Fullscreen Window", sf::Style::Fullscreen);
     window.setFramerateLimit(60);
     window.setPosition(sf::Vector2i(100, 100));
 
@@ -71,20 +73,18 @@ int main()
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
-            player.move();
+            player.Move(-1.0f * player.GetMoveSpeed());
         }
-        
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
-            player.move();    
+            player.Move(1.0f * player.GetMoveSpeed());
         }
         
         // Clear the window
         window.clear(sf::Color::Black);
 
-        player.player_sprite.setPosition(player.getPosition()); // Set the position of the player sprite
-
-        window.draw(player.player_sprite);    // Draw the player sprite
+        player.player_sprite.setPosition(player.GetPosition());
+        window.draw(player.player_sprite);
         
         // Display whatever you draw
         window.display();
