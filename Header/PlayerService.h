ba641a221cb2ba6a1f4aec02874f961f086c55a6
Parva <parvadesai404@gmail.com>
2024-06-17 16:12:09 +0530

    [Add] implemented player view & controller

--- Header/PlayerService.h
+++ Header/PlayerService.h
@@ -1,24 +1,11 @@
 ﻿#pragma once
-#include <SFML/Graphics.hpp>
+#include "../../Header/Player/PlayerController.h"
 
-class PlayerService 
+class PlayerService
 {
 private:
-    int health = 3;
-    sf::Vector2f position = sf::Vector2f(640.0f, 360.0f);
-    float movement_speed = 175.0f;
-    int player_score = 0;
-    
-    const sf::String player_texture_path = "assets/textures/player_ship.png";
-    
-    sf::Texture player_texture;
-    sf::Sprite player_sprite;
-    
-    sf::RenderWindow* game_window;
-     
-    void initializePlayerSprite();
-    void processPlayerInput();
-    
+    PlayerController* player_controller;
+
 public:
     PlayerService();
     ~PlayerService();
@@ -26,9 +13,4 @@ public:
     void initialize();
     void update();
     void render();
-		
-    void moveLeft();
-    void moveRight();
-    float getMoveSpeed() const;
-    sf::Vector2f getPosition() const;
 };
--- Source/PlayerService.cpp
+++ Source/PlayerService.cpp
@@ -1,68 +1,27 @@
 ﻿#include "../Header/PlayerService.h"
-#include "../Header/ServiceLocator.h"
+#include "../Header/Player/PlayerController.h"
 
 PlayerService::PlayerService()
 {
-    game_window = nullptr;
+    player_controller = new PlayerController();
 }
 
-PlayerService::~PlayerService() = default;
-
-void PlayerService::initialize() 
+PlayerService::~PlayerService()
 {
-    game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
-    initializePlayerSprite();	
+    delete (player_controller);
 }
 
-//take our players input in update, then set the position.
-//order is important here
+void PlayerService::initialize()
+{
+    player_controller->initialize();
+}
+
 void PlayerService::update()
 {
-    processPlayerInput();
-    player_sprite.setPosition(getPosition());
+    player_controller->update();
 }
 
 void PlayerService::render()
 {
-    game_window->draw(player_sprite);
+    player_controller->render();
 }
-
-void PlayerService::processPlayerInput()
-{
-    const EventService* event_service = ServiceLocator::getInstance()->getEventService();
-
-    // Check if a key has been pressed
-    if (event_service->isKeyboardEvent())
-    {
-        if (event_service->pressedLeftKey())
-        {
-            moveLeft();
-        }
-      
-        if (event_service->pressedRightKey())
-        {
-            moveRight();
-        }
-    }
-}
-
-void PlayerService::initializePlayerSprite()
-{
-    if (player_texture.loadFromFile(player_texture_path))
-    {
-        player_sprite.setTexture(player_texture);
-    }
-}
-
-void PlayerService::moveLeft()
-{
-    position.x -= movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-}
-
-void PlayerService::moveRight()
-{
-    position.x += movement_speed * ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
-}
-
-sf::Vector2f PlayerService::getPosition() const { return position; }
-float PlayerService::getMoveSpeed() const { return movement_speed; }
