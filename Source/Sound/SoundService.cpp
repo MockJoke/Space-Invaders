60c867d353064ba0c6fba534df983c7387240997
Parva <parvadesai404@gmail.com>
2024-06-30 01:50:13 +0530

    [Add] sound service

---
+++ Source/Sound/SoundService.cpp
@@ -0,0 +1,24 @@
+#include "../../header/Sound/SoundService.h"
+
+namespace Sound
+{
+    void SoundService::initialize()
+    {
+    }
+
+    void SoundService::loadBackgroundMusicFromFile()
+    {
+    }
+
+    void SoundService::loadSoundFromFile()
+    {
+    }
+
+    void SoundService::playSound(SoundType soundType)
+    {
+    }
+
+    void SoundService::playBackgroundMusic()
+    {
+    }
+}
