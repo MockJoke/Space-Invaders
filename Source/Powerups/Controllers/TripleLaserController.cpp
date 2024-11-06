cae8a6f5827bec32be7d485efe5fad0cc66898d0
kharva <parvadesai404@gmail.com>
2024-11-07 00:55:10 +0530

    [Fix] TripleLaserController class rename

---
+++ Source/Powerups/Controllers/TripleLaserController.cpp
@@ -0,0 +1,17 @@
+#include "../../header/Powerup/Controllers/TripleLaserController.h"
+#include "../../../Header/Global/ServiceLocator.h"
+
+namespace Powerup
+{
+    namespace Controller
+    {
+        TripleLaserController::TripleLaserController(PowerupType type) : PowerupController(type) {}
+
+        TripleLaserController::~TripleLaserController() {}
+
+        void TripleLaserController::applyPowerup()
+        {
+            Global::ServiceLocator::getInstance()->getPlayerService()->enableTripleLaser();
+        }
+    }
+}
