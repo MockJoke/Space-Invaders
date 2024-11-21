3afc2bff836c53c10130e8f52cfd4fbe182d6fa9
kharva <parvadesai404@gmail.com>
2024-11-22 02:08:19 +0530

    [Fix] resources path

--- src/Global/Config.cpp
+++ src/Global/Config.cpp
@@ -2,35 +2,35 @@
 
 namespace Global
 {
-	const sf::String Config::game_logo_texture_path = "../resources/Textures/game_logo.png";
-	const sf::String Config::background_texture_path = "../resources/Textures/game_bg.png";
-	const sf::String Config::player_texture_path = "../resources/Textures/player_ship.png";
+	const sf::String Config::game_logo_texture_path = "../../resources/Textures/game_logo.png";
+	const sf::String Config::background_texture_path = "../../resources/Textures/game_bg.png";
+	const sf::String Config::player_texture_path = "../../resources/Textures/player_ship.png";
 	
-	const sf::String Config::subzero_texture_path = "../resources/Textures/subzero.png";
-	const sf::String Config::zapper_texture_path = "../resources/Textures/zapper.png";
-	const sf::String Config::thunder_snake_texture_path = "../resources/Textures/thunder_snake.png";
-	const sf::String Config::ufo_texture_path = "../resources/Textures/ufo.png";
-	const sf::String Config::bunker_texture_path = "../resources/Textures/bunker.png";
+	const sf::String Config::subzero_texture_path = "../../resources/Textures/subzero.png";
+	const sf::String Config::zapper_texture_path = "../../resources/Textures/zapper.png";
+	const sf::String Config::thunder_snake_texture_path = "../../resources/Textures/thunder_snake.png";
+	const sf::String Config::ufo_texture_path = "../../resources/Textures/ufo.png";
+	const sf::String Config::bunker_texture_path = "../../resources/Textures/bunker.png";
 
-	const sf::String Config::explosion_texture_path = "../resources/textures/explosion.png";
+	const sf::String Config::explosion_texture_path = "../../resources/textures/explosion.png";
 	
-	const sf::String Config::shield_texture_path = "../resources/Textures/shield.png";
-	const sf::String Config::triple_laser_texture_path = "../resources/Textures/triple_laser.png";
-	const sf::String Config::rapid_fire_texture_path = "../resources/Textures/rapid_fire.png";
+	const sf::String Config::shield_texture_path = "../../resources/Textures/shield.png";
+	const sf::String Config::triple_laser_texture_path = "../../resources/Textures/triple_laser.png";
+	const sf::String Config::rapid_fire_texture_path = "../../resources/Textures/rapid_fire.png";
 	
-	const sf::String Config::player_bullet_texture_path = "../resources/Textures/player_bullet.png";
-	const sf::String Config::laser_bullet_texture_path = "../resources/Textures/laser_bullet.png";
-	const sf::String Config::torpedo_texture_path = "../resources/Textures/torpedoe.png";
-	const sf::String Config::frost_beam_texture_path = "../resources/Textures/frost_beam.png";
+	const sf::String Config::player_bullet_texture_path = "../../resources/Textures/player_bullet.png";
+	const sf::String Config::laser_bullet_texture_path = "../../resources/Textures/laser_bullet.png";
+	const sf::String Config::torpedo_texture_path = "../../resources/Textures/torpedoe.png";
+	const sf::String Config::frost_beam_texture_path = "../../resources/Textures/frost_beam.png";
 
-	const sf::String Config::menu_button_texture_path = "../resources/Textures/button.png";
+	const sf::String Config::menu_button_texture_path = "../../resources/Textures/button.png";
 
-	const sf::String Config::ArcadeI_font_path = "../resources/Fonts/ARCADE_I.ttf";
-	const sf::String Config::ArcadeN_font_path = "../resources/Fonts/ARCADE_N.ttf";
+	const sf::String Config::ArcadeI_font_path = "../../resources/Fonts/ARCADE_I.ttf";
+	const sf::String Config::ArcadeN_font_path = "../../resources/Fonts/ARCADE_N.ttf";
 
-	const sf::String Config::background_music_path = "../resources/Sounds/bg_music.mp3";
-	const sf::String Config::button_click_sound_path = "../resources/Sounds/button_click_sound.wav";
-	const sf::String Config::bullet_fire_sound_path = "../resources/sounds/bullet_fire.wav";
-	const sf::String Config::powerup_enabled_sound_path = "../resources/sounds/powerup_enabled.ogg";
-	const sf::String Config::powerup_disabled_sound_path = "../resources/sounds/powerup_disabled.ogg";
+	const sf::String Config::background_music_path = "../../resources/Sounds/bg_music.mp3";
+	const sf::String Config::button_click_sound_path = "../../resources/Sounds/button_click_sound.wav";
+	const sf::String Config::bullet_fire_sound_path = "../../resources/sounds/bullet_fire.wav";
+	const sf::String Config::powerup_enabled_sound_path = "../../resources/sounds/powerup_enabled.ogg";
+	const sf::String Config::powerup_disabled_sound_path = "../../resources/sounds/powerup_disabled.ogg";
 }
